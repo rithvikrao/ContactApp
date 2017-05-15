966ac57c59f2fde36d134ed1aad4f464f0d22afc
Rithvik Rao <rithvik321@gmail.com>
2017-05-15 10:27:42 -0700

    Things are inserted into database now!

--- MyContactApp/app/src/main/java/com/example/rithvik/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/rithvik/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.rithvik.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,6 +19,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String COL_2 = "NAME";
 
+    public static final String COL_3 = "AGE";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
@@ -31,5 +34,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
     }
 }
--- MyContactApp/app/src/main/java/com/example/rithvik/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/rithvik/mycontactapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.rithvik.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        //Add the layout vars
+        editName = (EditText) findViewById(R.id.editText_name);
+    }
+
+    public void addData(View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if(isInserted) {
+            Log.d("MyContact", "Success inserting data");
+            //Insert toast here...
+        }
+        else {
+            Log.d("MyContact", "Failure inserting data");
+            //Insert toast here...
+        }
+
     }
 }
